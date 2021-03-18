628d91b7344978c796652ac4dedddb3de462033f
Mina Ashna <mia@impact.dk>
2021-03-18 16:33:04 +0100

    initial commit

---
+++ Sizerium/Sizerium/SizeriumApp.swift
@@ -0,0 +1,17 @@
+//
+//  SizeriumApp.swift
+//  Sizerium
+//
+//  Created by Mina Ashna on 18/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct SizeriumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
